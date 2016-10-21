16dc576e0480f398c18a6207e05b40f2da2aa4c1
Lazlow Carmichael <lazlowcarmichael@gmail.com>
2016-10-21 00:21:38 -0400

    Add comments to pagedimmer.js

--- src/js/pagedimmer.js
+++ src/js/pagedimmer.js
@@ -1,8 +1,9 @@
-// Add div to page
+// Add overlay div to page
 function addOverlay() {
   $( 'body' ).append('"<div id="dimmerOverlay"></div>');
 }
 
+// Set position when scrolling
 function positionOverlay() {
   $( 'div#dimmerOverlay' ).css({
     top: $( window ).scrollTop(),
@@ -10,19 +11,21 @@ function positionOverlay() {
   });
 }
 
+// Set CSS styles of overlay
 function styleOverlay() {
   $( 'div#dimmerOverlay' ).css({
-    'pointer-events': 'none',
+    'pointer-events': 'none',      // Does not hijack clicks
     width: $( window ).width(),
     height: $( window ).height(),
     position: 'absolute',
     background: 'black',
-    'z-index': '9999',
+    'z-index': '9999',            // Stay on top
     top: 0,
     left: 0
   });
 }
 
+// Change size of overlay
 function resizeOverlay() {
   $( 'div#dimmerOverlay' ).css({
     width: $( window ).width(),
@@ -30,6 +33,7 @@ function resizeOverlay() {
   });
 }
 
+// Change opacity of overlay
 function adujustOverlayOpacity(opacity) {
   // Check valid opacity (0.00 - 1.00)
   if (opacity < 0 || opacity > 1) {
@@ -47,10 +51,12 @@ $( 'document' ).ready(function() {
   adujustOverlayOpacity(0.5);
 });
 
+// Reposition overlay on scroll
 window.addEventListener('scroll', function() {
   positionOverlay();
 });
 
+// Resize overlay on window resize
 window.addEventListener('resize', function() {
   resizeOverlay();
 });
